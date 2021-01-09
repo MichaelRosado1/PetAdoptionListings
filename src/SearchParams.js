e73a616587d2c086f66c1f200c2277ff7f7e3a9e
MichaelRosado1 <mrosad4@illinois.edu>
2021-01-08 20:28:58 -0600

    breed dropdown now updates when new animal is selected. Created dropdown component

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,76 +1,40 @@
-//import react
-import React, { useState, useEffect } from 'react';
-import pet, { ANIMALS } from '@frontendmasters/pet';
-import useDropdown from './useDropdown';
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
 
-//declare an arrow function called SearchParams
 const SearchParams = () => {
-    //destructuring the usestate object
-    //first item is always state and second is updater function
-    const [location, setLocation] = useState("Chicago, IL");
-    const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS); 
-    const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
-    const [breeds, setBreeds] = useState([]);
+  const [location, updateLocation] = useState("Seattle, WA");
+  const [breeds, updateBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, updateBreed] = useDropdown("Breed", "", breeds);
 
-    //runs whenever breed changes
-    useEffect(() => {
-        setBreeds([]);
-        setBreed("");
+  useEffect(() => {
+    updateBreeds([]);
+    updateBreed("");
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      updateBreeds(breedStrings);
+    }, console.error);
+  }, [animal]);
 
-        pet.breeds(animal).then(({ breeds }) => {
-            const breedStrings = breeds.map(({ name}) => name);
-            setBreeds(breedStrings);
-        }, console.error)
-    }, [animal, setBreed, setBreeds]);
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => updateLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
 
-    //return the html elements to display
-    return (
-        <div className="search-params"> 
-        <h1>{location}</h1>
-            <form>
-                <label htmlFor="location">
-                    location
-                    <input
-                    id="location"
-                    placeholder="Location" 
-                    value={location}
-                    onChange={ e => setLocation(e.target.value)}/>   
-                </label>    
-                <label htmlFor="animal">
-                    animal
-                    <select
-                        id="animal"
-                        value={animal}
-                        onChange={e => setAnimal(e.target.value)}
-                        onBlur={e => setAnimal(e.target.value)}>
-                        <option>All</option>
-                        {ANIMALS.map(animal => (
-                            <option key={animal} value={animal}>
-                                {animal}
-                            </option>
-                        ))}
-                    </select>
-                </label>
-                <label htmlFor="breed">
-                    breed
-                    <select
-                        id="breed"
-                        value={breed}
-                        onChange={e => setBreed(e.target.value)}
-                        onBlur={e => setBreed(e.target.value)}
-                        disabled={!breeds.length}
-                        >
-                            <option>All</option> 
-                            {breeds.map(breedString => (<option key={breedString} value={breedString}>
-                                {breedString}
-                            </option>))}
-                        </select>
-                </label>
-                <button>Submit</button>
-            </form> 
-        </div>
-    ) 
-}
-
-//if no specific functions are 'requested' export the entire class
 export default SearchParams;
