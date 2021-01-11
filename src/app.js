606f41c8ee52ead2ff1f44f90d57f87b27c6879d
MichaelRosado1 <mrosad4@illinois.edu>
2021-01-10 18:18:30 -0600

    added return home functionality and also used react router to go between pages

--- src/app.js
+++ src/app.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
 import SearchParams from './SearchParams';
+import { Link, Router } from "@reach/router";
+import Details from './Details';
 //app function
  const App = () => {
   return (
-    <div>
-      <h1 id="something-important">Adopt me!</h1>
-      <SearchParams />
-    </div>
-  )
+    <React.StrictMode>
+      <div>
+        <header>
+          <Link to="/">
+            <h1 id="something-important">Adopt Me!</h1>
+          </Link>
+        </header>
+       <Router>
+          <SearchParams path="/" />
+          <Details path="/details/1" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </React.StrictMode>
+ )
 };
 //renders the app to the browser
 render(<App />, document.getElementById('root'));
